8a34fca7865810467952e4d6662be371a7d4dccd
Kenan Casey <kenancasey@gmail.com>
2024-02-06 16:32:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def build_dictionary(words):
-    pass
+    print('Hey - 1')
+    print('you - 1')
 
 # REMOVE PASS AND FIX THE CODE TO:
 #1. asks for input, 
@@ -8,4 +9,5 @@ def build_dictionary(words):
 #3. calls build_dictionary() that will return a dictionary of each word in the input and it's frequency
 #4. output the contents of the dictionary sorted by key.
 if __name__ == '__main__':
-    pass
+    inp = input("enter something")
+    build_dictionary("Hey you")
